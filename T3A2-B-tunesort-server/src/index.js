bfa85147d257aa728532bb27a31517b147496304
shalforb <shalford@live.com.au>
2024-08-11 20:43:34 +1000

    vercel

--- T3A2-B-tunesort-server/src/index.js
+++ T3A2-B-tunesort-server/src/index.js
@@ -18,7 +18,7 @@ app.use('/users', userRoutes);
 app.use('/playlists', playlistRoutes);
 app.use('/spotify', spotifyRoutes); 
 
-const CONNECTION_URL = 'mongodb://localhost:27017/tunesort';
+const CONNECTION_URL = process.env.MONGODB_URI;
 mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.error(error.message));
