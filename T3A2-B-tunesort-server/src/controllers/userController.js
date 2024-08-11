9e2543bbae0798ed40e2d34260cd93bff7b1420a
shalforb <shalford@live.com.au>
2024-08-11 21:30:51 +1000

    bcrypt

--- T3A2-B-tunesort-server/src/controllers/userController.js
+++ T3A2-B-tunesort-server/src/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User'); // Import your User model
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const getUserInfo = async (req, res) => {
