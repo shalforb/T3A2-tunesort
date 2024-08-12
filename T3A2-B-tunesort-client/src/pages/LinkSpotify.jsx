889b61dec7df11c04ae0adb240d02fd47bacaa4c
shalforb <shalford@live.com.au>
2024-08-12 22:28:06 +1000

    styling

--- T3A2-B-tunesort-client/src/pages/LinkSpotify.jsx
+++ T3A2-B-tunesort-client/src/pages/LinkSpotify.jsx
@@ -3,24 +3,26 @@ import MainText from "../components/MainText";
 import Button from "../components/Button";
 import { FaSpotify } from "react-icons/fa";
 
-
-
 function LinkSpotify() {
-    
     const handleLinkSpotify = () => {
         window.location.href = `${import.meta.env.VITE_SERVER_BASE_URL}/spotify/authorize`;
     };
-    return (
-        <div>
-            <NavBar />
-            <div className="flex items-center justify-center h-64">
-            <FaSpotify className="text-[210px] text-green-600" />
-            </div>
-            <MainText mainText="Tunesort is powered by Spotify's API, link your account below to get started." className="text-[36px]" />
-            <div className="flex justify-center">
-            <Button buttonText="Link Your Spotify" className=" py-8 px-36 bg-green-600 text-[24px] text-white rounded-md" onClick={handleLinkSpotify} />
-            </div>
 
+    return (
+        <div className="min-h-screen flex flex-col justify-between">
+            <NavBar />
+            <div className="flex-grow flex flex-col items-center justify-center text-center p-4">
+                <FaSpotify className="text-[150px] md:text-[210px] text-green-600" />
+                <MainText 
+                    mainText="Tunesort is powered by Spotify's API, link your account below to get started." 
+                    className="text-[24px] md:text-[36px] max-w-2xl"
+                />
+                <Button 
+                    buttonText="Link Your Spotify" 
+                    className="mt-8 py-4 px-16 md:py-6 md:px-24 bg-green-600 hover:bg-green-700 text-[18px] md:text-[24px] text-white rounded-md" 
+                    onClick={handleLinkSpotify} 
+                />
+            </div>
         </div>
     );
 }
